a3c6cacef05d116abe5fbbe99c43503596923ce5
jzalacainllanes <jzalacainllanes@gmail.com>
2021-08-17 06:50:56 -0400

    New Provider tests

--- bin/pllink.sh
+++ bin/pllink.sh
@@ -4,7 +4,7 @@
 # #%L
 # prolobjectlink-jpi-ip-xsb
 # %%
-# Copyright (C) 2020 Prolobjectlink Project
+# Copyright (C) 2020 - 2021 Prolobjectlink Project
 # %%
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
